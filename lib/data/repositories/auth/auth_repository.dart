84cae5cd089595656a4890d27638507d0e29476f
natanzeraa <contato@natanfelipe.com.br>
2025-09-01 23:38:20 -0300

    feat: add auth_repository

--- lib/data/repositories/auth/auth_repository.dart
+++ lib/data/repositories/auth/auth_repository.dart
@@ -1,13 +1,41 @@
-import 'package:flutter/foundation.dart';
+import 'package:mobile_app/data/services/api/auth_service.dart';
+import 'package:mobile_app/data/services/models/auth_request/auth_request.dart';
+import 'package:mobile_app/data/services/models/auth_response/auth_response.dart';
+import 'package:mobile_app/utils/result.dart';
 
-import '../../../utils/result.dart';
+class AuthRepository {
+  final AuthService service;
+  AuthRepository(this.service);
 
-abstract class AuthRepository extends ChangeNotifier {
-  /// Returns true when the user is logged in
-  /// Returns [Future] because it will load a stored auth state the first time.
-  Future<bool> get isAuthenticated;
+  Future<bool> get isAuthenticated => Future.value(false);
 
-  Future<Result<void>> login({required String email, required String password});
+  Future<Result<void>> login({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final request = AuthRequest(email: email, password: password);
+      final result = await service.login(request);
+      return result;
+    } on Exception catch (e) {
+      return Result.error(e);
+    }
+  }
 
-  Future<Result<void>> logout();
+  Future<Result<AuthResponse>> signup(AuthRequest request) async {
+    final result = await service.signup(request);
+    return result;
+  }
+
+  Future<Result<void>> logout() async {
+    return const Result.ok(null);
+  }
+
+  String? getTokenSync() {
+    return service.getTokenSync();
+  }
+
+  Future<void> loadToken() async {
+    await service.loadToken();
+  }
 }
