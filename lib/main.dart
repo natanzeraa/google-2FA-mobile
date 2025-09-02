e5fc94b3456439d8ff729e02fc6a34a519520570
Natan Felipe <contato@natanfelipe.com.br>
2025-09-02 14:07:55 -0300

    refactor: update main.dart to make it cleaner

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_app/data/repositories/auth/auth_repository.dart';
-import 'package:mobile_app/data/services/api/auth_service.dart';
 import 'package:mobile_app/routing/router.dart';
 import 'package:mobile_app/ui/core/themes/theme.dart';
 import 'package:mobile_app/ui/core/ui/scroll_behavior.dart';
-import 'package:mobile_app/ui/login/view_model/login_view_model.dart';
+import 'package:mobile_app/ui/auth/view_model/auth_view_model.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
   runApp(
-    MultiProvider(
-      providers: [
-        Provider<AuthRepository>(create: (_) => AuthRepository(AuthService())),
-        ChangeNotifierProvider<LoginViewModel>(
-          create: (context) =>
-              LoginViewModel(repository: context.read<AuthRepository>()),
-        ),
-      ],
+    ChangeNotifierProvider(
+      create: (_) => AuthViewModel(),
       child: const MyApp(),
-    ),
+    )
   );
 }
 
