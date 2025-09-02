a458d9b5355f88ad7ed76685ea4abeba942f22c9
Natan Felipe <contato@natanfelipe.com.br>
2025-09-02 14:09:28 -0300

    feat: add repository for handling appointments request and response

--- lib/data/repositories/appointments/appointments_repository.dart
+++ lib/data/repositories/appointments/appointments_repository.dart
@@ -1,1 +1,20 @@
-class CustomersRepository {}
+import 'package:mobile_app/data/repositories/appointments/appointments_repository.dart';
+import 'package:mobile_app/data/services/local/local_service.dart';
+import 'package:mobile_app/domain/models/appointment/appointment.dart';
+import 'package:mobile_app/utils/result.dart';
+
+class AppointmentsRepository extends AppointmentsRepository {
+  AppointmentsRepository({required AppointmentsService service})
+    : _service = service;
+
+  final service _service;
+
+  Future<Result<List<Appointment>>> getAppointments() async {
+    try {
+      final appointments = await _service.getAppointments();
+      return Result.ok(appointments);
+    } on Exception catch (error) {
+      return Result.error(error);
+    }
+  }
+}
