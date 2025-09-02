3b9179f2b02b7ec73e56522b937ffcf5da967297
Natan Felipe <contato@natanfelipe.com.br>
2025-09-02 14:05:46 -0300

    refactor: delete unnecessary files

--- lib/ui/signup/view_model/signup_view_model.dart
+++
@@ -1,6 +0,0 @@
-import 'package:mobile_app/data/repositories/auth/auth_repository.dart';
-import 'package:flutter/material.dart';
-
-class SignupViewModel extends ChangeNotifier {
-  SignupViewModel({required AuthRepository authRepository});
-}
