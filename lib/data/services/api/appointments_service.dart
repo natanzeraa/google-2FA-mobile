9b6d71d5cb73de090d6a8fc7dfb218808a70a7dd
Natan Felipe <contato@natanfelipe.com.br>
2025-09-02 14:10:08 -0300

    feat: add getAppointments method to AppointmentsService

--- lib/data/services/api/appointments_service.dart
+++ lib/data/services/api/appointments_service.dart
@@ -1,49 +1,36 @@
-//   typedef AuthHeaderProvider = String? Function();
+typedef AuthHeaderProvider = String? Function();
 
-// class AppointmentsService {
+class Assets {
+  static const appointments = 'assets/appointmens.json';
+}
 
-//   ApiClient({String? host, int? port, HttpClient Function()? clientFactory})
-//     : _host = host ?? 'localhost',
-//       _port = port ?? 8080,
-//       _clientFactory = clientFactory ?? HttpClient.new;
+class AppointmentsService {
 
-//   final String _host;
-//   final int _port;
-//   final HttpClient Function() _clientFactory;
+  ApiClient({String? host, int? port, HttpClient Function()? clientFactory})
+    : _host = host ?? 'localhost',
+      _port = port ?? 8080,
+      _clientFactory = clientFactory ?? HttpClient.new;
 
-//   AuthHeaderProvider? _authHeaderProvider;
+  final String _host;
+  final int _port;
+  final HttpClient Function() _clientFactory;
 
-//   set authHeaderProvider(AuthHeaderProvider authHeaderProvider) {
-//     _authHeaderProvider = authHeaderProvider;
-//   }
+  AuthHeaderProvider? _authHeaderProvider;
 
-//   Future<void> _authHeader(HttpHeaders headers) async {
-//     final header = _authHeaderProvider?.call();
-//     if (header != null) {
-//       headers.add(HttpHeaders.authorizationHeader, header);
-//     }
-//   }
+  set authHeaderProvider(AuthHeaderProvider authHeaderProvider) {
+    _authHeaderProvider = authHeaderProvider;
+  }
 
-//   Future<Result<List<Continent>>> getContinents() async {
-//     final client = _clientFactory();
-//     try {
-//       final request = await client.get(_host, _port, '/continent');
-//       await _authHeader(request.headers);
-//       final response = await request.close();
-//       if (response.statusCode == 200) {
-//         final stringData = await response.transform(utf8.decoder).join();
-//         final json = jsonDecode(stringData) as List<dynamic>;
-//         return Result.ok(
-//           json.map((element) => Continent.fromJson(element)).toList(),
-//         );
-//       } else {
-//         return const Result.error(HttpException("Invalid response"));
-//       }
-//     } on Exception catch (error) {
-//       return Result.error(error);
-//     } finally {
-//       client.close();
-//     }
-//   }
+  Future<void> _authHeader(HttpHeaders headers) async {
+    final header = _authHeaderProvider?.call();
+    if (header != null) {
+      headers.add(HttpHeaders.authorizationHeader, header);
+    }
+  }
 
-// }
+  Future<List<Appointment>> getAppointments() async {
+    final json = await _loadStringAsset(Assets.appointments);
+    return json.map<Appointment>(Appointment.fromJson).toList();
+  }
+
+}
