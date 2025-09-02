fc720658799e4f53344c1ee8a621230efc9a72ad
natanzeraa <contato@natanfelipe.com.br>
2025-09-01 23:40:40 -0300

    feat: update home screen with button for logout and title

--- lib/ui/home/widgets/home_screen.dart
+++ lib/ui/home/widgets/home_screen.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          children: [
+            Text("Home"),
+            TextButton(
+              onPressed: () => context.go("/login"),
+              child: Text("Sair", style: TextStyle(color: Colors.red)),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
