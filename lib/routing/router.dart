bf6f9c5ab4d40d2044b21c91c0ccecb58928e556
natanzeraa <contato@natanfelipe.com.br>
2025-09-02 15:43:12 -0300

    refactor: renamed router

--- lib/routing/router.dart
+++ lib/routing/router.dart
@@ -1,37 +1,33 @@
 import 'package:go_router/go_router.dart';
-import 'package:mobile_app/data/repositories/auth/auth_repository.dart';
-import 'package:mobile_app/data/services/api/auth_service.dart';
+import 'package:mobile_app/ui/auth/view_model/auth_view_model.dart';
+import 'package:mobile_app/ui/auth/widgets/login_screen.dart';
+import 'package:mobile_app/ui/auth/widgets/signup_screen.dart';
 import 'package:mobile_app/ui/home/widgets/home_screen.dart';
-import 'package:mobile_app/ui/login/view_model/login_view_model.dart';
-import 'package:mobile_app/ui/login/widgets/login_screen.dart';
-import 'package:mobile_app/ui/signup/view_model/signup_view_model.dart';
-import 'package:mobile_app/ui/signup/widgets/signup_screen.dart';
-import 'package:provider/provider.dart';
 
 import 'routes.dart';
 
-final auth = context.watch<AuthViewModel>();
+GoRouter router(AuthViewModel auth) {
+  return GoRouter(
+    initialLocation: Routes.login,
+    refreshListenable: auth,
 
-final router = GoRouter(
-  initialLocation: '/login',
-  refreshListenable: auth, 
+    redirect: (context, state) {
+      final loggedIn = auth.isLoggedIn;
+      final loggingIn = state.matchedLocation == Routes.login;
 
-  redirect: (context, state) {
-    final loggedIn = auth.isLoggedIn;
-    final loggingIn = state.matchedLocation == '/login';
+      if (!loggedIn && !loggingIn) {
+        return Routes.login;
+      }
+      if (loggedIn && loggingIn) {
+        return Routes.home;
+      }
+      return null;
+    },
 
-    if (!loggedIn && !loggingIn) {
-      return '/login';  
-    }
-    if (loggedIn && loggingIn) {
-      return '/home'; 
-    }
-    return null; 
-  },
-
-   routes: [
-    GoRoute(path: Routes.login, builder: (context, state) => const LoginScreen()),
-    GoRoute(path: Routes.signup, builder: (context, state) => const SignupScreen()),
-    GoRoute(path: Routes.home, builder: (context, state) => const HomeScreen()),
-  ],
-);
+    routes: [
+      GoRoute(path: Routes.login, builder: (context, state) => const LoginScreen()),
+      GoRoute(path: Routes.signup, builder: (context, state) => const SignupScreen()),
+      GoRoute(path: Routes.home, builder: (context, state) => const HomeScreen()),
+    ],
+  );
+}
